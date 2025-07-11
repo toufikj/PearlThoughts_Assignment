70a511400c34c6d35b51b46a231d4dffce48574b
toufikjamadar335@gmail.com <toufikjamadar335@gmail.com>
2025-07-11 16:51:16 +0530

    second commit

--- modules/aws/ecs_v3/main.tf
+++ modules/aws/ecs_v3/main.tf
@@ -4,71 +4,21 @@ resource "aws_cloudwatch_log_group" "ecs" {
   retention_in_days = 7
 }
 
-# Store each required value as a separate secret
-resource "aws_secretsmanager_secret" "port" {
-  name = "${var.product}-port"
+resource "aws_secretsmanager_secret" "database_url" {
+  name = "${var.product}-database-url"
 }
-resource "aws_secretsmanager_secret_version" "port" {
-  secret_id     = aws_secretsmanager_secret.port.id
-  secret_string = tostring(var.container_port)
-}
-
-resource "aws_secretsmanager_secret" "postgres_host" {
-  name = "${var.product}-postgres-host"
-}
-resource "aws_secretsmanager_secret_version" "postgres_host" {
-  secret_id     = aws_secretsmanager_secret.postgres_host.id
-  secret_string = aws_db_instance.postgres.address
+resource "aws_secretsmanager_secret_version" "database_url" {
+  secret_id     = aws_secretsmanager_secret.database_url.id
+  secret_string = "postgres://${aws_db_instance.postgres.username}:${random_password.postgres.result}@${aws_db_instance.postgres.address}:${aws_db_instance.postgres.port}/${aws_db_instance.postgres.db_name}"
 }
 
-resource "aws_secretsmanager_secret" "postgres_port" {
-  name = "${var.product}-postgres-port"
+resource "aws_secretsmanager_secret" "redis_url" {
+  name = "${var.product}-redis-url"
 }
-resource "aws_secretsmanager_secret_version" "postgres_port" {
-  secret_id     = aws_secretsmanager_secret.postgres_port.id
-  secret_string = tostring(aws_db_instance.postgres.port)
+resource "aws_secretsmanager_secret_version" "redis_url" {
+  secret_id     = aws_secretsmanager_secret.redis_url.id
+  secret_string = "redis://${aws_elasticache_cluster.redis.cache_nodes[0].address}:${aws_elasticache_cluster.redis.cache_nodes[0].port}"
 }
-
-resource "aws_secretsmanager_secret" "postgres_db" {
-  name = "${var.product}-postgres-db"
-}
-resource "aws_secretsmanager_secret_version" "postgres_db" {
-  secret_id     = aws_secretsmanager_secret.postgres_db.id
-  secret_string = aws_db_instance.postgres.db_name
-}
-
-resource "aws_secretsmanager_secret" "postgres_user" {
-  name = "${var.product}-postgres-user"
-}
-resource "aws_secretsmanager_secret_version" "postgres_user" {
-  secret_id     = aws_secretsmanager_secret.postgres_user.id
-  secret_string = aws_db_instance.postgres.username
-}
-
-resource "aws_secretsmanager_secret" "postgres_password" {
-  name = "${var.product}-postgres-password"
-}
-resource "aws_secretsmanager_secret_version" "postgres_password" {
-  secret_id     = aws_secretsmanager_secret.postgres_password.id
-  secret_string = aws_db_instance.postgres.password
-}
-
-resource "aws_secretsmanager_secret" "redis_host" {
-  name = "${var.product}-redis-host"
-}
-resource "aws_secretsmanager_secret_version" "redis_host" {
-  secret_id     = aws_secretsmanager_secret.redis_host.id
-  secret_string = aws_elasticache_cluster.redis.cache_nodes[0].address
-}
-
-resource "aws_secretsmanager_secret" "redis_port" {
-  name = "${var.product}-redis-port"
-}
-resource "aws_secretsmanager_secret_version" "redis_port" {
-  secret_id     = aws_secretsmanager_secret.redis_port.id
-  secret_string = tostring(aws_elasticache_cluster.redis.cache_nodes[0].port)
-}
-
 # Create ECS Task Definition for Strapi
 resource "aws_ecs_task_definition" "ecs" {
   family                   = "${var.stage}-${var.product}-task"
@@ -79,87 +29,50 @@ resource "aws_ecs_task_definition" "ecs" {
   memory                   = var.memory
 
   container_definitions = jsonencode([{
-    name      = var.container_name
-    image     = var.container_image_uri
-    essential = true
-    portMappings = [{
-      containerPort = var.container_port
-      protocol      = var.container_protocol
-    }]
-    environment = concat(
-      [
-        {
-          name  = "DISABLE_MEDUSA_ADMIN"
-          value = "false"
-        },
-        {
-          name  = "MEDUSA_WORKER_MODE"
-          value = "server"
-        },
-        {
-          name  = "PORT"
-          value = tostring(var.container_port)
-        },
-        {
-          name  = "POSTGRES_HOST"
-          value = aws_db_instance.postgres.address
-        },
-        {
-          name  = "POSTGRES_PORT"
-          value = tostring(aws_db_instance.postgres.port)
-        },
-        {
-          name  = "POSTGRES_DB"
-          value = aws_db_instance.postgres.db_name
-        },
-        {
-          name  = "POSTGRES_USER"
-          value = aws_db_instance.postgres.username
-        },
-        {
-          name  = "POSTGRES_PASSWORD"
-          value = aws_db_instance.postgres.password
-        },
-        {
-          name  = "REDIS_HOST"
-          value = aws_elasticache_cluster.redis.cache_nodes[0].address
-        },
-        {
-          name  = "REDIS_PORT"
-          value = tostring(aws_elasticache_cluster.redis.cache_nodes[0].port)
-        }
-      ]
-    )
-    # Inject ARNs of secrets for DB and Redis
-    secrets = [
-      { name = "PORT"             valueFrom = aws_secretsmanager_secret.port.arn },
-      { name = "POSTGRES_HOST"    valueFrom = aws_secretsmanager_secret.postgres_host.arn },
-      { name = "POSTGRES_PORT"    valueFrom = aws_secretsmanager_secret.postgres_port.arn },
-      { name = "POSTGRES_DB"      valueFrom = aws_secretsmanager_secret.postgres_db.arn },
-      { name = "POSTGRES_USER"    valueFrom = aws_secretsmanager_secret.postgres_user.arn },
-      { name = "POSTGRES_PASSWORD" valueFrom = aws_secretsmanager_secret.postgres_password.arn },
-      { name = "REDIS_HOST"       valueFrom = aws_secretsmanager_secret.redis_host.arn },
-      { name = "REDIS_PORT"       valueFrom = aws_secretsmanager_secret.redis_port.arn }
-    ]
-    logConfiguration = {
-      logDriver = "awslogs"
-      options = {
-        "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
-        "awslogs-region"        = var.region
-        "awslogs-stream-prefix" = var.stage
-      }
+  name      = var.container_name
+  image     = var.container_image_uri
+  essential = true
+  portMappings = [{
+    containerPort = var.container_port
+    protocol      = var.container_protocol
+  }]
+  environment = [
+    {
+      name  = "NODE_ENV"
+      value = "production"
+    },
+    {
+      name  = "DISABLE_MEDUSA_ADMIN"
+      value = "true"
+    },
+    {
+      name  = "MEDUSA_WORKER_MODE"
+      value = "server"
+    },
+    {
+      name  = "PORT"
+      value = tostring(var.container_port)
     }
-  }])
-  depends_on = [
-    aws_secretsmanager_secret_version.port,
-    aws_secretsmanager_secret_version.postgres_host,
-    aws_secretsmanager_secret_version.postgres_port,
-    aws_secretsmanager_secret_version.postgres_db,
-    aws_secretsmanager_secret_version.postgres_user,
-    aws_secretsmanager_secret_version.postgres_password,
-    aws_secretsmanager_secret_version.redis_host,
-    aws_secretsmanager_secret_version.redis_port
   ]
+  secrets = [
+    {
+      name      = "DATABASE_URL"
+      valueFrom = aws_secretsmanager_secret.database_url.arn
+    },
+    {
+      name      = "REDIS_URL"
+      valueFrom = aws_secretsmanager_secret.redis_url.arn
+    }
+  ]
+  logConfiguration = {
+    logDriver = "awslogs"
+    options = {
+      "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
+      "awslogs-region"        = var.region
+      "awslogs-stream-prefix" = var.stage
+    }
+  }
+}])
 }
 
 resource "aws_ecs_cluster" "ecs" {
